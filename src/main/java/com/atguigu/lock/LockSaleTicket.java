131026f63b94814acdeec24fba6b3e0464969eea
fengzhentian <359051902@qq.com>
2022-04-03 21:44:31 +0800

    Lock接口实现案例

--- src/main/java/com/atguigu/lock/LockSaleTicket.java
+++ src/main/java/com/atguigu/lock/LockSaleTicket.java
@@ -1,20 +1,32 @@
-package com.atguigu.sync;
+package com.atguigu.lock;
+
+import java.util.concurrent.locks.ReentrantLock;
 
 // 第一步，创建资源类，定义属性和操作方法
 class Ticket {
     // 票数量
     private int number = 30;
 
+    // 创建可重入锁
+    private final ReentrantLock lock = new ReentrantLock();
+
     // 操作方法：买票
-    public synchronized void sale() {
-        // 判断：是否有票
-        if (number > 0) {
-            System.out.println(Thread.currentThread().getName() + " ：卖出：" + (number--) + " 剩下：" + number);
+    public void sale() {
+        // 上锁
+        lock.lock();
+        try {
+            // 判断：是否有票
+            if (number > 0) {
+                System.out.println(Thread.currentThread().getName() + " ：卖出：" + (number--) + " 剩下：" + number);
+            }
+        } finally {
+            // 解锁
+            lock.unlock();
         }
     }
 }
 
-public class SaleTicket {
+public class LockSaleTicket {
     // 第二步，创建多个线程，调用资源类的操作方法
     public static void main(String[] args) {
         // 创建Ticket对象
